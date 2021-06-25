b8de6b829194cad5dedf4b71e3d88f25d8f860c4
aannuuj <hariompalkar@gmail.com>
2021-06-25 22:58:29 +0530

    feat: razorpay + form

---
+++ model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const chillieCustomer = new mongoose.Schema({
+    name: String,
+    amount: Number,
+    email: String,
+    address: String,
+    postalCode: Number,
+    phoneNumber: String,
+    suggestions: String,
+    paymentId: String,
+    orderId: String,
+    signature: String
+})
+module.exports = mongoose.model('customer', chillieCustomer);
+
---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const Razorpay = require('razorpay');
+const shortid = require('shortid');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://adminID:hariom@cluster0.i9rco.mongodb.net/chillyDatabse?retryWrites=true&w=majority');
+
+mongoose.connection.once('open', function () {
+    console.log('Database is now connected');
+}).on('error', function (error) {
+    console.log('error');
+});
+const chillieCustomer = require('./model');
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+const razorpay = new Razorpay({
+    key_id: 'rzp_test_81hEx0Xv1jjwNu',
+    key_secret: 'PagfD1OJKU3jS1SUQPtyLV9h'
+})
+
+app.post('/verification',(req, res) => {
+    res.json({status: 'ok'});
+    const secret = '12345678';
+    //console.log(req.body.payload.payment);
+    const crypto = require('crypto');
+    const shasum = crypto.createHmac('sha256',secret);
+    shasum.update(JSON.stringify(req.body));
+    const digest = shasum.digest('hex');
+    if(digest === req.headers['x-razorpay-signature']){
+        console.log('request is correct');
+    }
+    else{
+        console.log('false request');
+    }
+})
+
+app.post('/razorpay', async (req, res) => {
+    const paymentCapture = 1;
+    const amount = req.body.amount;
+    const currency = 'INR';
+    //console.log(req.body)
+    const options = {
+        amount: amount*100,
+        currency: currency,
+        receipt: shortid.generate(),
+        payment_capture: paymentCapture
+    }
+    try{
+        const response = await razorpay.orders.create(options);
+        res.json({
+            id: response.id,
+            currency: 'INR',
+            amount: response.amount
+        })
+    }catch(error){
+        console.log(error)
+    }
+    //console.log('order id has been sent');
+})
+app.post('/formData', (req, res) => {
+    const customer = new chillieCustomer(req.body)
+    customer.save()
+        .then(() => console.log("data saved to database"))
+        .catch(err => console.log(err))
+})
+
+app.post('/success', (req, res) => {
+    res.sendFile('./sucess.html')
+})
+
+app.listen(1337, () => {
+    console.log('Listening to port 1337')
+});
